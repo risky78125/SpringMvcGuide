dc57debaf2931aa77964761edb2313c4fc7e4327
Risky57 <risky5200@gmail.com>
2019-05-22 22:55:07 +0800

    增加RequestHeader, CookieValue, RequestAttribute, SessionAttribute

--- src/main/java/com/wuqi/dev/controller/ParameterController.java
+++ src/main/java/com/wuqi/dev/controller/ParameterController.java
@@ -1,7 +1,8 @@
 package com.wuqi.dev.controller;
 
+import com.wuqi.dev.entity.BookInfo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestAttributes;
 
@@ -10,6 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/params")
 public class ParameterController {
+
+    /*
+    向Request和Session中存一些数据, 方便下面某些请求进行读取测试
+     */
+    @RequestMapping("/req0")
+    public String req0(NativeWebRequest request){
+        BookInfo book = new BookInfo();
+        book.setBookId(1001L);
+        book.setBookName("三国演义");
+        request.setAttribute("requestBook", book, RequestAttributes.SCOPE_REQUEST);
+        request.setAttribute("sessionBook", book, RequestAttributes.SCOPE_SESSION);
+        return "forward:req4";
+    }
 
     /*
     使用NativeWebRequest对象与使用HttpServletRequest一样,
@@ -44,6 +58,53 @@ public class ParameterController {
         return "success";
     }
 
+    /*
+    获取某个请求头数据
+     */
+    @RequestMapping("/req2")
+    public String req2(@RequestHeader("User-Agent") String userAgent){
+        return "success";
+    }
+
+
+    /*
+    获取Cookie中某条数据
+     */
+    @RequestMapping("/req3")
+    public String req3(@CookieValue("JSESSIONID") String sessionId){
+        return "success";
+    }
+
+    /*
+    从请求域中获取数据.
+    required表示该数据是否为必需的.
+    默认值为true, 如果从域中无法取出数据, 则会返回400响应码.
+    下面的SessionAttribute同理.
+     */
+    @RequestMapping("/req4")
+    public String req4(@RequestAttribute(value = "requestBook", required = false) BookInfo book){
+        return "success";
+    }
+
+    /*
+    从Session中获取数据
+     */
+    @RequestMapping("/req5")
+    public String req5(@SessionAttribute("sessionBook") BookInfo book){
+        return "success";
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/wuqi/dev/CommonsTest.java
+++ src/test/java/com/wuqi/dev/CommonsTest.java
@@ -1,13 +1,11 @@
 package com.wuqi.dev;
 
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
 
 public class CommonsTest {
 
     @Test
     public void test() {
-
     }
 
 }
